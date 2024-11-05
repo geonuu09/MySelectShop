cab20b39725bcf87408c9850ff1c72aed69345bc
LeeGeonu <rjsdndl909@gmail.com>
2024-11-05 22:03:45 +0900

    관심상품 조회 API

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -7,7 +7,6 @@ import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
 import java.util.ArrayList;
 import java.util.List;
-import javax.sound.sampled.Port;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
